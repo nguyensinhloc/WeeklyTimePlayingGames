e527084f1ec3055dd70055c27bfd3cf5d3da10d9
Nguyễn Sinh Lộc <locnguyen1005@gmail.com>
2023-10-17 21:15:01 +0700

    Modify existing code, then add a bar chart

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the CSV file
-df = pd.read_csv('WeeklyTimePlayingGames.csv')
+# Read the data from a CSV file
+df = pd.read_csv("WeeklyTimePlayingGames.csv")
 
-# Handle missing values (replace NaN with mean for numeric columns)
-df.fillna(df.mean(), inplace=True)
+# clean data
+age = df["age"].tolist()
+df = df[df["age"] < 60]
+index = pd.read_csv("WeeklyTimePlayingGames.csv")
+df.to_csv('file_output.csv', index=False)
+df = pd.read_csv('file_output.csv')
 
-# Remove unnecessary columns (if any)
-columns_to_keep = ['player_id', 'first_name', 'last_name', 'age', 'email', 'country', 'gender', 'favorite_game', 'weekly_hours']
-df_cleaned = df[columns_to_keep]
+# Create a bar chart
+weekly_hours = df["weekly_hours"].tolist()
+country = df["country"].tolist()
+x_axis = ['country']
+y_axis = ['weekly_hours']
+plt.figure(figsize=(10, 6))
+plt.bar(country, weekly_hours)
+plt.title('weekly hours playing video games')
+plt.xlabel('country')
+plt.ylabel('weekly_hours')
+plt.xticks(rotation=90)
+plt.show()
 
-# Rename columns
-new_column_names = {'player_id': 'Player ID', 'first_name': 'First Name', 'last_name': 'Last Name',
-                    'age': 'Age', 'email': 'Email', 'country': 'Country',
-                    'gender': 'Gender', 'favorite_game': 'Favorite Game', 'weekly_hours': 'Weekly Hours'}
-df_cleaned.rename(columns=new_column_names, inplace=True)
+# Group by 'favorite_game' and count occurrences
+games_counts = df['favorite_game'].value_counts()
 
-# Group by the "Favorite Game" column and calculate the total weekly hours
-game_popularity = df.groupby("Favorite Game")["Weekly Hours"].sum()
-
-# Create a 3D pie chart
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, projection="3d")
-
-# Plot the pie chart
-ax.pie(game_popularity, labels=game_popularity.index, autopct="%1.1f%%", startangle=90)
-
-# Set title and labels
-ax.set_title("Popularity of Games")
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
-
+# Create a pie chart
+plt.pie(games_counts, labels=games_counts.index, autopct='%1.1f%%')
+plt.axis('equal')  # Ensure the chart is circular
+plt.title('Favorite Games by Country')  # Chart title
 plt.show()
 
